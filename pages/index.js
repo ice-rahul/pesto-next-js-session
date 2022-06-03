0c7903e39b69a28fa3cea2b969f87130db137a1b
Rahul Agrawal <rahul.agrawal.con@hotstar.com>
2022-06-04 01:41:40 +0530

    static route

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,43 +13,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div style={{display: 'flex', gap: '20px'}}>
+          <Link href='/'>Home</Link>
+          <Link href='page1'>Page 1</Link>
+          <Link href='page2'>Page 2</Link>
+          <Link href='page3'>Page 3</Link>
+          <Link href='page4'>Page 4</Link>
+          <Link href='page5'>Page 5</Link>
         </div>
       </main>
 
---
+++ pages/page4.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'
+
+function Page4() {
+  return (
+    <main className={styles.main}>
+        Page 4 <br /> <br />
+      <div style={{ display: 'flex', gap: '20px' }}>
+        <Link href="/">Home</Link>
+        <Link href="page1">Page 1</Link>
+        <Link href="page2">Page 2</Link>
+        <Link href="page3">Page 3</Link>
+        <Link href="page4">Page 4</Link>
+        <Link href="page5">Page 5</Link>
+      </div>
+    </main>
+  )
+}
+
+export default Page4
